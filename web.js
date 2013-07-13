b88a0099de5134a35ff2fde922757024920a21ab
Ubuntu <ubuntu@ip-172-31-10-219.eu-west-1.compute.internal>
2013-07-13 21:27:48 +0000

    Changed web.js

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
+var fs = require('fs');
+var buff = new buffer();
+
+buff = fs.readFileSync("index.html");
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+  var text =  buff.toString();
 });
 
 var port = process.env.PORT || 5000;
